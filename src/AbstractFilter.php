04a7030b2f5ded55d87fe8b61f9a634f5bc5899e
ExenJer <exenjers@gmail.com>
2019-05-17 01:08:06 +0300

    Added base logic. Added cast to type

---
+++ src/AbstractFilter.php
@@ -0,0 +1,181 @@
+<?php
+
+
+namespace ExenJer\LaravelFilters;
+
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class AbstractFilter
+{
+    /**
+     * Current model.
+     *
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Allowed request fields for filtering.
+     *
+     * @var array
+     */
+    protected $fields = [];
+
+    /**
+     * Cast field to the type
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * @var bool
+     */
+    protected $withDeletions = false;
+
+    /**
+     * Current query builder for the model.
+     *
+     * @var Builder
+     */
+    private $builder;
+
+    /**
+     * Current filter class.
+     *
+     * @var self
+     */
+    private $filter;
+
+    /**
+     * AbstractFilter constructor.
+     */
+    public function __construct()
+    {
+        $this->setUp();
+    }
+
+    /**
+     * Main process of filtering.
+     *
+     * @param array $request Array of request key - values
+     * @return Collection
+     */
+    protected function apply(array $request): Collection
+    {
+        $this->fieldsCheck($request);
+
+        return $this->builder->get();
+    }
+
+    private function fieldsCheck(array $input): void
+    {
+        foreach ($this->fields as $field) {
+            if (array_key_exists($field, $input)) {
+                $this->caller($field, $input);
+            }
+        }
+    }
+
+    private function caller(string $key, array $input, ?string $value = null): void
+    {
+        if (!$value) {
+            $value = $input[$key];
+        }
+
+        $this->castValueType($key, $value);
+
+        method_exists($this->filter, $key . 'Filter') ?
+            call_user_func([$this->filter, $key . 'Filter'], $value)
+            : $this->defaultFilterCall($key, $value);
+    }
+
+    /**
+     * Default filter.
+     *
+     * @param string $field
+     * @param mixed $value
+     * @return Builder
+     */
+    protected function defaultFilterCall(string $field, $value): Builder
+    {
+        return $this->builder
+            ->where($field, $value);
+    }
+
+    /**
+     * @return self
+     */
+    protected function getFilter(): self
+    {
+        return $this->filter;
+    }
+
+    /**
+     * @param $filter
+     */
+    protected function setFilter(self $filter): void
+    {
+        $this->filter = $filter;
+    }
+
+    /**
+     * Initialize default data.
+     *
+     * @return void
+     */
+    private function setUp(): void
+    {
+        $this->builder = $this->model::query();
+    }
+
+    /**
+     * Cast value to the type by cast array.
+     *
+     * @param string $field
+     * @param $value
+     */
+    private function castValueType(string $field, &$value): void
+    {
+        if (array_key_exists($field, $this->casts)) {
+            $type = $this->casts[$field];
+
+            switch ($type) {
+                case 'integer':
+                case 'int':
+                    $value = (int) $value;
+                    break;
+                case 'boolean':
+                case 'bool':
+                    $value = (bool) $value;
+                    break;
+                case 'float':
+                    $value = (float) $value;
+                    break;
+                case 'double':
+                    $value = (double) $value;
+                    break;
+                case 'real':
+                    $value = (real) $value;
+                    break;
+                case 'array':
+                    $value = (array) $value;
+                    break;
+                case 'object':
+                    $value = (object) $value;
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @return Builder
+     */
+    public function __invoke(): Builder
+    {
+        return $this->builder;
+    }
+}
